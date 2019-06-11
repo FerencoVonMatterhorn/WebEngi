b47a6208df16e026657a1d4a9698ac1ed02cb565
Bengt Joachimsohn <bengt.joachimsohn@bewotec.de>
2019-06-11 16:45:40 +0200

    DBActions aufgeräumt
    Hilfsmethoden umbenannt und nach unten in der Klasse verlagert

--- Webengineering/src/main/java/servlets/IndexLoggedInServlet.java
+++ Webengineering/src/main/java/servlets/IndexLoggedInServlet.java
@@ -24,7 +24,7 @@ public class IndexLoggedInServlet extends HttpServlet {
 		RequestDispatcher rd;
 		UserPojo user = DBActions.findUserById((int) req.getSession().getAttribute("userID"));
 
-		req.setAttribute("paymentPojo", DBActions.getPaymentForIndexLoggedIn(user.getId()));
+		req.setAttribute("paymentPojo", DBActions.findPaymentForIndexLoggedInByUserId(user.getId()));
 		req.setAttribute("userPojo", user);
 		req.setAttribute("groupPojo", DBActions.indexLoggedInGroup(user.getId()));
 
